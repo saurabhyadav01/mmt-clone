bee8edf8bb3301d4e298e7965465f6e30cc25578
Mohit Singhal <singhalmohit011@gmail.com>
2022-04-01 14:58:28 +0530

    Home Page updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import {HomeNavbar} from "./components/HomePage/HomeNavbar/HomeNavbar"
 import { HomePageBookings } from './components/HomePage/HomePageBookings/HomePageBookings';
 import { Footer } from './components/HomePage/Footer/Footer';
 import { SecondFooter } from './components/HomePage/Footer/SecondFooter';
+import { MainContentFooter } from './components/HomePage/MainContentFooter/MainContentFooter';
+import { MainContent } from './components/HomePage/MainContent/MainContent';
 function App() {
   return (
-    <div className="App">
+    <div>
       <HomeNavbar/>
       <HomePageBookings/>
+      <MainContent/>
       <SecondFooter/>
       <Footer/>
     </div>
---
+++ src/components/HomePage/MainContent/MainContentAppDownload/MainContentAppDownload.jsx
@@ -0,0 +1,37 @@
+import "./MainContentAppDownload.css";
+
+export const MainContentAppDownload = ()=>{
+    return <div>
+        <div>
+            <img src="https://mmt.servedbyadbutler.com/getad.img/;libID=3406718" alt="" />
+        </div>
+          <div className="home-page-maincontent-app">
+        <div>
+            <h1>Download App Now !</h1>
+            <h3>Get India's #1 travel super app, join 100 Million+ happy travellers!</h3>
+        </div>
+        <div className="homepage-main-content-app-download-details">
+            <div>
+                <div></div>
+                <div>
+                <p>Use code WELCOMEMMT and get upto Rs 1200 off on your first domestic flight booking</p>
+                <input type="text" placeholder="Enter Mobile Number" />
+                <button>GET APP LINK</button>
+                </div>
+            </div>
+            <div>
+                <div>
+                <p>MORE WAYS TO GET THE APP</p>
+                </div>
+                <div>
+                <div></div>
+                <div className="homepage-main-content-app-download-details-qrCode">
+                    <img src="https://promos.makemytrip.com/notification/xhdpi/QRCodeDT_QR-code.jpg" alt="" />
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  
+}
