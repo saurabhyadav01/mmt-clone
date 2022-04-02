d31018192e05f7a5494237d8a809e987533e5a05
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-04-02 21:15:52 +0530

    some changes

--- src/components/HomePage/MainContent/MainContentCities/MainContentCites.jsx
+++ src/components/HomePage/MainContent/MainContentCities/MainContentCites.jsx
@@ -1,39 +1,61 @@
 import "./MainContentCitie.css"
 export const MainContentCites=()=>{
     return <div className="Famous-Cities">
-    <div>
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
-         <div><h3>Chennai Flights</h3></div>
-         <p>via- Delhi,Mumbai,Bangolre</p>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+         
     </div>
-    <div className="flex">
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
          <div><h3>Chennai Flights</h3>
          <p>via- Delhi,Mumbai,Bangolre</p></div>
          
     </div>
     
-    <div>
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
-         <div><h3>Chennai Flights</h3></div>
-         <p>via- Delhi,Mumbai,Bangolre</p>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+        
     </div>
-    <div>
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
-         <div><h3>Chennai Flights</h3></div>
-         <p>via- Delhi,Mumbai,Bangolre</p>
+         <div><h3>Chennai Flights
+         </h3><p>via- Delhi,Mumbai,Bangolre</p></div>
+         
     </div>
-    <div>
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
-         <div><h3>Chennai Flights</h3></div>
-         <p>via- Delhi,Mumbai,Bangolre</p>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+         
     </div>
-    <div>
+    <div className="Famous-Cities-flex">
         <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
-         <div><h3>Chennai Flights</h3></div>
-         <p>via- Delhi,Mumbai,Bangolre</p>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+         
     </div>
-
+    <div className="Famous-Cities-flex">
+        <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
+         <div><h3>Chennai Flights
+         </h3><p>via- Delhi,Mumbai,Bangolre</p></div>
+         
+    </div>
+    <div className="Famous-Cities-flex">
+        <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+         
+    </div>
+    <div className="Famous-Cities-flex">
+        <div><img src="https://promos.makemytrip.com/images/50x50-Other-23052019.png" alt="" /></div>
+         <div><h3>Chennai Flights</h3>
+         <p>via- Delhi,Mumbai,Bangolre</p></div>
+         
+    </div>
     
 
     </div>
