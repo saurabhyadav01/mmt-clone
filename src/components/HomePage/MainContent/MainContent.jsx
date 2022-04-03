32f60f54ed59363ff1545f40c36104618eaf366e
Mohit Singhal <singhalmohit011@gmail.com>
2022-04-03 19:49:10 +0530

    project completed

--- src/components/HomePage/MainContent/MainContent.jsx
+++ src/components/HomePage/MainContent/MainContent.jsx
@@ -2,7 +2,7 @@ import { MainContentFooter } from "../MainContentFooter/MainContentFooter"
 import { MainContentAppDownload } from "./MainContentAppDownload/MainContentAppDownload"
 import { MainContentTrip } from "./MainContentTrip/MainContentTrip"
 import {MainContentCites} from "./MainContentCities/MainContentCites"
-import {CarouselApplication} from "../carousel/carousel";
+// import {CarouselApplication} from "../carousel/carousel";
 import ExploreSearch from "./ExploreSearch/ExploreSearch";
 import {MainContentOffers} from "./MainContentOffers/MainContentOffers";
 import "./MainContent.css";
